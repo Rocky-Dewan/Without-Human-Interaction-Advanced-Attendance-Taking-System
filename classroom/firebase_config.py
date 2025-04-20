8e4a20551c339b852d9bc92f75e3f797973315fd
Rocky Dewan <76462396+Rocky-Dewan@users.noreply.github.com>
2025-04-20 13:25:24 +0600

    Update firebase_config.py

--- classroom/firebase_config.py
+++ classroom/firebase_config.py
@@ -4,7 +4,10 @@ import firebase_config
 from firebase_admin import credentials, db
 
 # Initialize Firebase
-
+cred = credentials.Certificate("iot-attendance-system-914b1-firebase-adminsdk-fbsvc-c832ed67dc.json")  # Use your downloaded key
+firebase_admin.initialize_app(cred, {
+    "databaseURL": "https://iot-attendance-system-914b1-default-rtdb.firebaseio.com/"  # Change this to your Firebase URL
+})
 
 # Reference to the database
 students_ref = db.reference('students')
