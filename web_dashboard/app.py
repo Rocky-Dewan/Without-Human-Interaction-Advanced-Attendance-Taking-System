8213715aa6a0021d983740c992a412468eb72cb4
Rocky Dewan <76462396+Rocky-Dewan@users.noreply.github.com>
2025-04-20 13:31:06 +0600

    Update app.py

--- web_dashboard/app.py
+++ web_dashboard/app.py
@@ -1,12 +1,44 @@
-from flask import Flask, render_template
-from firebase_config import ref
+from flask import Flask, render_template, jsonify, request, redirect, session
+import firebase_admin
+from firebase_admin import credentials, db
+import os
+
+# Initialize Firebase
+cred = credentials.Certificate("iot-attendance-system-914b1-firebase-adminsdk-fbsvc-c832ed67dc.json")
+firebase_admin.initialize_app(cred, {
+    "databaseURL": "https://iot-attendance-system-914b1-default-rtdb.firebaseio.com//"
+})
 
 app = Flask(__name__)
 
-@app.route("/")
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        if request.form['username'] == 'admin' and request.form['password'] == 'admin123':
+            session['logged_in'] = True
+            return redirect('/')
+        return "Invalid credentials"
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    return redirect('/login')
+
+@app.route('/')
 def index():
-    students = ref.get()
-    return render_template("index.html", students=students)
+
+    return render_template('index.html')
+
+
+@app.route('/data')
+def data():
+    attendance_ref = db.reference("attendance")
+    attendance_data =  attendance_ref.get()
+    return jsonify(attendance_data)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
+
