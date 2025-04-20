fdfa0be583d63bf1e7eb46569efe4855f13e6762
Rocky Dewan <76462396+Rocky-Dewan@users.noreply.github.com>
2025-04-20 13:31:47 +0600

    Update add_sample_students.py

--- add_sample_students.py
+++ add_sample_students.py
@@ -2,19 +2,22 @@ import firebase_admin
 from firebase_admin import credentials, db
 
 # Initialize Firebase
-#collect from me ..
+cred = credentials.Certificate("iot-attendance-system-914b1-firebase-adminsdk-fbsvc-7af87f1206.json")  # Use your downloaded key
+firebase_admin.initialize_app(cred, {
+    "databaseURL": "https://iot-attendance-system-914b1-default-rtdb.firebaseio.com/"  # Change this to your Firebase URL
+})
 
 # Sample Student Data
 students_data = {
     "1001": {
-        "name": "John Doe",
-        "barcode": "123456789",
-        "image_url": "https://example.com/john.jpg"
+        "name": "Rocky",
+        "barcode": "1234",
+        "image_url": "rocky.jpg"
     },
     "1002": {
-        "name": "Alice Smith",
-        "barcode": "987654321",
-        "image_url": "https://example.com/alice.jpg"
+        "name": "Dewan",
+        "barcode": "4321",
+        "image_url": "dewan.jpg"
     }
 }
 
